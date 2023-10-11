0a973c5ad0c54127f8e1af6b3187bde7d78d7197
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2023-10-11 15:34:50 +0200

    added the case where we add the solution for left and right incident waves. Resonators are now centered around 0.

--- left_right_incwave.m
+++ left_right_incwave.m
@@ -13,7 +13,7 @@ L = sum(li)+sum(lij); % length of the unit cell
 Ls = zeros(2*N-1,1);
 Ls(1:2:end) = li;
 Ls(2:2:end) = lij;
-xipm = [0,cumsum(Ls)']+spacing; % all boundary points
+xipm = [0,cumsum(Ls)']-(len*(N/2)+spacing*(N-1)/2); % all boundary points
 xm = xipm(1:2:end); % LHS boundary points
 xp = xipm(2:2:end); % RHS boundary points
 z = (xm+xp)./2; % centers of resonators
@@ -53,13 +53,14 @@ k_op = w_op/v0; % operating wave number outside of the resonator
 k0 = w0/v0; % wave number of incident frequency
 
 % Define relevant functions
-% uin_l = @(x,t,n) exp(sqrt(-1)*((k0).*x+w0.*t)).*(x<xm(1)).*(n==0); % left incident wave
-% vin_l = @(x,n) exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
-% dx_vin_l = @(x,n) sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
-uin_l = @(x,t,n) 0; vin_l = @(x,n) 0; dx_vin_l = @(x,n) 0;
+uin_l = @(x,t,n) exp(sqrt(-1)*((k0).*x+w0.*t)).*(x<xm(1)).*(n==0); % left incident wave
+vin_l = @(x,n) exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
+dx_vin_l = @(x,n) sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
+% uin_l = @(x,t,n) 0; vin_l = @(x,n) 0; dx_vin_l = @(x,n) 0;
 uin_r = @(x,t,n) exp(sqrt(-1)*(-(k0).*x+w0.*t)).*(x>xp(end)).*(n==0); % right incident wave
 vin_r = @(x,n) exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % n-th mode of right incident wave
 dx_vin_r = @(x,n) -sqrt(-1)*k0*exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % derivative of right incident wave
+% uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
 G = @(k,x) exp(sqrt(-1)*k*abs(x))./(2*sqrt(-1)*k); % Green's function
 
 % Define evaluation points
@@ -114,4 +115,90 @@ for i = 1:N
     subplot(1,2,2)
     plot(zs(i,:),imag(us_eval_z),'r-','DisplayName','Exact',markersize=8,linewidth=2)
 end
+
+%% Add solution for left incident wave and right incident wave
+
+% left incident wave
+uin_l = @(x,t,n) exp(sqrt(-1)*((k0).*x+w0.*t)).*(x<xm(1)).*(n==0); % left incident wave
+vin_l = @(x,n) exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
+dx_vin_l = @(x,n) sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
+uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
+
+% Compute solution coefficients
+MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
+MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
+sol_l = MatcalA\MatcalF; % solve for the interior coefficients, vector \mathbf{w}
+
+us_eval_x_l = zeros(1,len_xs);
+us_eval_z_l = zeros(N,len_zs);
+usx_l = @(x) N*(uin_l(x,t,0)+uin_r(x,t,0)) + get_us_lr(x, t, N, xm, xp, lij, k_tr, v0, w_op, Omega, rs, ks, vr, sol_l, w_res, k0, vin_l, vin_r); % scattered wave field as a function of x for fixed time t, according to formula (31)
+
+for i = 1:len_xs
+    us_eval_x_l(i) = usx_l(xs(i));
+end
+
+for i = 1:N
+    for j = 1:len_zs
+        us_eval_z_l(i,j) = usx_l(zs(i,j));
+    end
+end
+
+
+% right incident wave
+uin_l = @(x,t,n) 0; vin_l = @(x,n) 0; dx_vin_l = @(x,n) 0;
+uin_r = @(x,t,n) exp(sqrt(-1)*(-(k0).*x+w0.*t)).*(x>xp(end)).*(n==0); % right incident wave
+vin_r = @(x,n) exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % n-th mode of right incident wave
+dx_vin_r = @(x,n) -sqrt(-1)*k0*exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % derivative of right incident wave
+
+% Compute solution coefficients
+MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
+MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
+sol_r = MatcalA\MatcalF; % solve for the interior coefficients, vector \mathbf{w}
+
+us_eval_x_r = zeros(1,len_xs);
+us_eval_z_r = zeros(N,len_zs);
+usx_r = @(x) N*(uin_l(x,t,0)+uin_r(x,t,0)) + get_us_lr(x, t, N, xm, xp, lij, k_tr, v0, w_op, Omega, rs, ks, vr, sol_r, w_res, k0, vin_l, vin_r); % scattered wave field as a function of x for fixed time t, according to formula (31)
+
+for i = 1:len_xs
+    us_eval_x_r(i) = usx_r(xs(i));
+end
+
+for i = 1:N
+    for j = 1:len_zs
+        us_eval_z_r(i,j) = usx_r(zs(i,j));
+    end
+end
+
+% add left and right solutions
+us_eval_z = us_eval_z_l+us_eval_z_r;
+us_eval_x = us_eval_x_l+us_eval_x_r;
+
+% create plot
+fig = figure();
+fig.Position = [263,725,982,352];
+subplot(1,2,1)
+set(gca,'FontSize',14)
+hold on
+plot(xs,real(us_eval_x),'-','DisplayName','Exact',markersize=8,linewidth=2)
+xlim([xs(1) xs(end)])
+xlabel('$x$',Interpreter='latex',FontSize=18)
+ylabel(strcat('Re$(u^{\mathrm{sc}}(x,$',num2str(t),'$))$'),Interpreter='latex',FontSize=18)
+
+subplot(1,2,2)
+set(gca,'FontSize',14)
+hold on
+plot(xs,imag(us_eval_x),'-','DisplayName','Exact',markersize=8,linewidth=2)
+xlim([xs(1) xs(end)])
+xlabel('$x$',Interpreter='latex',FontSize=18)
+ylabel(strcat('Im$(u^{\mathrm{sc}}(x,$',num2str(t),'$))$'),Interpreter='latex',FontSize=18)
+
+for i = 1:N
+    subplot(1,2,1)
+    plot(zs(i,:),real(us_eval_z(i,:)),'r-','DisplayName','Exact',markersize=8,linewidth=2)
+    subplot(1,2,2)
+    plot(zs(i,:),imag(us_eval_z(i,:)),'r-','DisplayName','Exact',markersize=8,linewidth=2)
+end
+
+
+
 
