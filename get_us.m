6e19e275834644c74ecf7c28c923e586f675ca63
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2023-11-15 12:45:47 +0100

    coorected the error by factor N

--- get_us.m
+++ get_us.m
@@ -1,4 +1,4 @@
-function [us] = get_us(x, t, N, xim, xip, lij, k_tr, v0, w, Omega, rs, ks, vr, sol, w_res, kin, v_in)
+function [us] = get_us(x, t, N, xim, xip, lij, k_tr, v0, w_op, Omega, rs, ks, vr, sol, w_res, kin, v_in)
 %GET_US Get the scattered wave evaluated at x at time t.
 %   x:      evaluation point
 %   t:      evaluation time
@@ -8,7 +8,7 @@ function [us] = get_us(x, t, N, xim, xip, lij, k_tr, v0, w, Omega, rs, ks, vr, s
 %   lij:    spacing between the resonators
 %   k_tr:   truncation parameter
 %   v0:     wave speed outside of the resonators
-%   w:      operating frequency \omega
+%   w_op:   operating frequency \omega
 %   Omega:  period of time modulations
 %   rs:     Fourier coefficients of \rho_i(t)
 %   ks:     Fourier coefficients of \kappa_i(t)
@@ -19,16 +19,9 @@ function [us] = get_us(x, t, N, xim, xip, lij, k_tr, v0, w, Omega, rs, ks, vr, s
 %   v_in:   incident wave field, function of x and n
 
     us = 0;
-    for j = 1:N
-        for n = -k_tr:k_tr
-%             if n == 0
-%                 v_in = @(p) 1*exp(sqrt(-1)*(kin).*p).*(p<xim(1));
-%             else
-%                 v_in = @(p) 0;
-%             end
-            kn = (w+n*Omega)/v0;
-            us = us + operator_S(x, N, xim, xip, lij, k_tr, kn, w, Omega, rs, ks, vr, sol, n, kin, v_in)*exp(sqrt(-1)*(n*Omega+w_res(j))*t);
-        end
+    for n = -k_tr:k_tr
+        kn = (w_op+n*Omega)/v0;
+        us = us + operator_S(x, N, xim, xip, lij, k_tr, kn, w_op, Omega, rs, ks, vr, sol, n, kin, v_in)*exp(sqrt(-1)*(n*Omega+w_op)*t);
     end
 
 end
