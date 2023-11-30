fbbc8ce2c47ef44d2b6156edd30702ccbc710cdd
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2023-11-30 15:55:05 +0100

    added plots depending on omega and epsilon

--- test_energy_conservation.m
+++ test_energy_conservation.m
@@ -3,9 +3,9 @@ format long
 
 % Settings for the material's structure
 k_tr = 4; % truncation parameters as in remark 3.3
-N = 1; % number of the resonator
+N = 6; % number of the resonator
 spacing = 10; lij = ones(1,N-1).*spacing; % spacing between the resonators
-len = 1; li = ones(1,N).*len; % length of the resonator
+len = 2; li = ones(1,N).*len; % length of the resonator
 L = sum(li)+sum(lij); % length of the unit cell
 Ls = zeros(2*N-1,1);
 Ls(1:2:end) = li;
@@ -28,7 +28,7 @@ for i = 1:(N-1)
     phase_kappa(i+1) = pi/i;
     phase_rho(i+1) = pi/i;
 end
-epsilon_kappa = 0; % modulation amplitude of kappa
+epsilon_kappa = 0.3; % modulation amplitude of kappa
 epsilon_rho = 0; % modulation amplitude of rho
 rs = []; % Fourier coefficients of 1/rho
 ks = []; % Fourier coefficients of 1/kappa
@@ -47,54 +47,124 @@ else
     w_muller = get_capacitance_approx_hot(epsilon_kappa,li,Omega,phase_kappa,delta,C,vr,v0,lij,xm,xp); % subwavelength resonant frequencies
     w_res = w_muller(real(w_muller)>=0); % positive subwavelength resonant frequencies
 end
-w_op = w_res(1)+ 0.0002; % operating frequency
-k_op = w_op/v0; % operating wave number outside of the resonator
-w0 = w_op; k0 = k_op;
-G = @(k,x) exp(sqrt(-1)*k*abs(x))./(2*sqrt(-1)*k); % Green's function
-dx_G = @(k,x) x*exp(sqrt(-1)*k*abs(x))./(2*sqrt(-1)*abs(x)); % Derivative of Green's function
 
+%% Iterate over omega
+
+all_w = linspace(delta,sqrt(delta),10);
+O = zeros(2*k_tr+1,length(all_w)); i = 1;
 % fig = figure();
 % hold on
-% c_map = parula(2*k_tr+1+2); % colors for plot
+c_map = parula(length(all_w)+2);
+
+for w_op = all_w(2:end)
+    
+    k_op = w_op/v0; % operating wave number outside of the resonator
+    w0 = w_op; k0 = k_op;
+    G = @(k,x) exp(sqrt(-1)*k*abs(x))./(2*sqrt(-1)*k); % Green's function
+    dx_G = @(k,x) x*exp(sqrt(-1)*k*abs(x))./(2*sqrt(-1)*abs(x)); % Derivative of Green's function
+    
+    % Define incident wave field  
+    vin_l = @(x,n) 1*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
+    dx_vin_l = @(x,n) 1*sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
+    % uin_l = @(x,t,n) 0; vin_l = @(x,n) 0; dx_vin_l = @(x,n) 0;
+    % uin_r = @(x,t,n) exp(sqrt(-1)*(-(k0).*x+w0.*t)).*(x>xp(end)).*(n==0); % right incident wave
+    % vin_r = @(x,n) exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % n-th mode of right incident wave
+    % dx_vin_r = @(x,n) -sqrt(-1)*k0*exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % derivative of right incident wave
+    uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
+    
+    % Build the linear system and solve it for the interior coefficients
+    MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
+    MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
+    N_vin = getN_vin(k_tr, N, delta, xm, xp, w_op, Omega, v0, lij, vin_l, vin_r); % matrix-vector product \mathcal{N}v^{in}
+    RHS = MatcalF + N_vin;
+    sol = MatcalA\RHS; % solve for the interior coefficients, vector \mathbf{w}
+    
+    % Compute the transmission and reflection coefficients
+    [tns,rns] = get_tr(k_tr, w_op, Omega, rs, ks, vr, xm, xp, sol, lij, vin_l, vin_r);
+    O(:,i) = abs(tns(:,1)).^2+abs(rns(:,end)).^2;
+    i = i+1;
+
+    % Create plot
+    semilogy(-k_tr:k_tr,O(:,i-1),'o','Color',c_map(i-1,:),'DisplayName',strcat('$\omega=$',num2str(w_op)),'MarkerSize',8,'LineWidth',2)
+    hold on
+
+end
+
+xlabel('$n$',Interpreter='latex',FontSize=18)
+ylabel('$|R_n|^2+|T_n|^2$',Interpreter='latex',FontSize=18)
+legend('show',interpreter='latex',fontsize=18)
 
 
-w_op = w0;
-k_op = w_op/v0;
-k0 = w0/v0; % wave number of incident frequency
+%% Iterate over epsilon_kappa
 
-% Define incident wave field
+all_epsk = [0.1,0.2,0.3,0.4,0.5,0.6];
+O = zeros(2*k_tr+1,length(all_epsk)); ic = 1;
+% fig = figure();
+% hold on
+c_map = parula(length(all_epsk)+2);
 
-vin_l = @(x,n) 1*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
-dx_vin_l = @(x,n) 1*sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
-% uin_l = @(x,t,n) 0; vin_l = @(x,n) 0; dx_vin_l = @(x,n) 0;
-% uin_r = @(x,t,n) exp(sqrt(-1)*(-(k0).*x+w0.*t)).*(x>xp(end)).*(n==0); % right incident wave
-% vin_r = @(x,n) exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % n-th mode of right incident wave
-% dx_vin_r = @(x,n) -sqrt(-1)*k0*exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % derivative of right incident wave
-uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
+for epsilon_kappa = all_epsk
 
-% Build the linear system and solve it for the interior coefficients
-MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
-MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
-N_vin = getN_vin(k_tr, N, delta, xm, xp, w_op, Omega, v0, lij, vin_l, vin_r); % matrix-vector product \mathcal{N}v^{in}
-RHS = MatcalF + N_vin;
-sol = MatcalA\RHS; % solve for the interior coefficients, vector \mathbf{w}
+    % Setting for the material's time-modulation
+    Omega = 0.03; % modulation frequency
+    T = 2*pi/Omega;
+    phase_kappa = zeros(1,N); % modulation phases of kappa
+    phase_rho = zeros(1,N); % modulation phases of rho
+    for i = 1:(N-1)
+        phase_kappa(i+1) = pi/i;
+        phase_rho(i+1) = pi/i;
+    end
+    epsilon_rho = 0; % modulation amplitude of rho
+    rs = []; % Fourier coefficients of 1/rho
+    ks = []; % Fourier coefficients of 1/kappa
+    for j = 1:N
+        rs_j = [epsilon_rho*exp(-1i*phase_rho(j))./2,1,epsilon_rho*exp(1i*phase_rho(j))./2];
+        ks_j = [epsilon_kappa*exp(-1i*phase_kappa(j))./2,1,epsilon_kappa*exp(1i*phase_kappa(j))./2];
+        ks = [ks; ks_j];
+        rs = [rs; rs_j];
+    end
+    
+    % Calculate subwavelength resonant frequency
+    if N == 1
+        w_res = get_capacitance_approx_spec_im_N1_1D(epsilon_kappa,Omega,len,delta,vr,v0); % non-zero subwavelength resonant frequency
+    else
+        C = make_capacitance_finite(N,lij); % capacitance matrix
+        w_muller = get_capacitance_approx_hot(epsilon_kappa,li,Omega,phase_kappa,delta,C,vr,v0,lij,xm,xp); % subwavelength resonant frequencies
+        w_res = w_muller(real(w_muller)>=0); % positive subwavelength resonant frequencies
+    end
+    w_op = w_res(1)+0.0002; w0 = w_op;
+    k_op = w_op/v0; k0 = w0/v0;
+    
+    
+    % Define incident wave field  
+    vin_l = @(x,n) 1*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
+    dx_vin_l = @(x,n) 1*sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
+    % uin_l = @(x,t,n) 0; vin_l = @(x,n) 0; dx_vin_l = @(x,n) 0;
+    % uin_r = @(x,t,n) exp(sqrt(-1)*(-(k0).*x+w0.*t)).*(x>xp(end)).*(n==0); % right incident wave
+    % vin_r = @(x,n) exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % n-th mode of right incident wave
+    % dx_vin_r = @(x,n) -sqrt(-1)*k0*exp(sqrt(-1)*(-(k0).*x)).*(x>xp(end)).*(n==0); % derivative of right incident wave
+    uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
+    
+    % Build the linear system and solve it for the interior coefficients
+    MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
+    MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
+    N_vin = getN_vin(k_tr, N, delta, xm, xp, w_op, Omega, v0, lij, vin_l, vin_r); % matrix-vector product \mathcal{N}v^{in}
+    RHS = MatcalF + N_vin;
+    sol = MatcalA\RHS; % solve for the interior coefficients, vector \mathbf{w}
+    
+    % Compute the transmission and reflection coefficients
+    [tns,rns] = get_tr(k_tr, w_op, Omega, rs, ks, vr, xm, xp, sol, lij, vin_l, vin_r);
+    O(:,ic) = abs(tns(:,1)).^2+abs(rns(:,end)).^2;
+    ic = ic+1;
 
-% Compute the transmission and reflection coefficients
-[tns,rns] = get_tr(k_tr, w_op, Omega, rs, ks, vr, xm, xp, sol, lij, vin_l, vin_r);
-O = abs(tns).^2+abs(rns).^2;
+    % Create plot
+    semilogy(-k_tr:k_tr,O(:,ic-1),'o','Color',c_map(ic-1,:),'DisplayName',strcat('$\varepsilon_{\kappa}=\,\,$',num2str(epsilon_kappa)),'MarkerSize',8,'LineWidth',2)
+    hold on
 
-% Create plot of the transmission and reflection coefficients
-%     for i = 1:N
-%         figure(i)
-%         hold on
-%     end
+end
 
-
-
-
-
-% xlabel('$\omega_{\mathrm{in}}$',Interpreter='latex',FontSize=18)
-% ylabel('$\Lambda_n$',Interpreter='latex',FontSize=18)
-% legend('show',interpreter='latex',fontsize=18)
+xlabel('$n$',Interpreter='latex',FontSize=18)
+ylabel('$|R_n|^2+|T_n|^2$',Interpreter='latex',FontSize=18)
+legend('show',interpreter='latex',fontsize=18)
 
 
