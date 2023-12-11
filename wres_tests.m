c89f5273f5f0c4b20c957cba477f5f68d82993e4
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2023-12-11 10:07:30 +0100

    added new plots for energycons

--- wres_tests.m
+++ wres_tests.m
@@ -4,7 +4,7 @@ format long
 
 % Settings for the material's structure
 k_tr = 4; % truncation parameters as in remark 3.3
-N = 2; % number of the resonator
+N = 1; % number of the resonator
 % L = 2000; % length of the domain \mathcal{U}
 % spacing = L/N; lij = ones(1,N-1).*spacing; % spacing between the resonators
 spacing = 10; lij = ones(1,N-1).*spacing; % spacing between the resonators
@@ -29,8 +29,8 @@ T = 2*pi/Omega;
 phase_kappa = zeros(1,N); % modulation phases of kappa
 phase_rho = zeros(1,N); % modulation phases of rho
 for i = 1:(N-1)
-    phase_kappa(i+1) = pi/i;
-    phase_rho(i+1) = pi/i;
+    phase_kappa(i+1) = pi;
+    phase_rho(i+1) = pi;
 end
 epsilon_rho = 0; % modulation amplitude of rho
 
@@ -43,7 +43,7 @@ tol = 1e-10;
 % function to iterate with
 p = @(ek) find_imagzero(ek,li,Omega,phase_kappa,delta,vr,v0,lij);
 
-eks = linspace(0,4,800); p_eks = zeros(1,length(eks)); k = 1;
+eks = linspace(0,3,800); p_eks = zeros(1,length(eks)); k = 1;
 for ek = eks
     p_eks(k) = p(ek);
     k = k+1;
@@ -54,6 +54,46 @@ end
 % find minimum
 [min_val,idx] = min(p_eks);
 min_eks = eks(idx);
+
+%% Compute energy for epsilon st Im(omega)=0
+
+epsilon_kappa = min_eks; % modulation amplitude of kappa
+rs = []; % Fourier coefficients of 1/rho
+ks = []; % Fourier coefficients of 1/kappa
+for j = 1:N
+    rs_j = [epsilon_rho*exp(-1i*phase_rho(j))./2,1,epsilon_rho*exp(1i*phase_rho(j))./2];
+    ks_j = [epsilon_kappa*exp(-1i*phase_kappa(j))./2,1,epsilon_kappa*exp(1i*phase_kappa(j))./2];
+    ks = [ks; ks_j];
+    rs = [rs; rs_j];
+end
+
+% Calculate subwavelength resonant frequency
+if N == 1
+    w_res = get_capacitance_approx_spec_im_N1_1D(epsilon_kappa,Omega,len,delta,vr,v0); % non-zero subwavelength resonant frequency
+else
+    C = make_capacitance_finite(N,lij); % capacitance matrix
+    w_muller = get_capacitance_approx_hot(epsilon_kappa,li,Omega,phase_kappa,delta,C,vr,v0); % subwavelength resonant frequencies
+    w_res = w_muller(real(w_muller)>=0); % positive subwavelength resonant frequencies
+end
+w_op = w_res; w0 = w_op;
+k_op = w_op/v0; k0 = k_op;
+
+% Define incident wave field  
+vin_l = @(x,n) 1*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
+dx_vin_l = @(x,n) 1*sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
+uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
+
+% Build the linear system and solve it for the interior coefficients
+MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
+MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
+N_vin = getN_vin(k_tr, N, delta, xm, xp, w_op, Omega, v0, lij, vin_l, vin_r); % matrix-vector product \mathcal{N}v^{in}
+RHS = MatcalF + N_vin;
+sol = MatcalA\RHS; % solve for the interior coefficients, vector \mathbf{w}
+
+% Calculate energy
+[tns,rns] = get_tr(k_tr, w_op, Omega, rs, ks, vr, xm, xp, sol, lij, vin_l, vin_r);
+O = abs(tns(:,1)).^2+abs(rns(:,end)).^2;
+semilogy(-k_tr:k_tr,O,'*g','DisplayName',strcat('$\varepsilon_{\kappa}=\,\,$',num2str(epsilon_kappa)),'MarkerSize',8,'LineWidth',2)
 
 %% Determine regions
 
@@ -121,12 +161,13 @@ if N == 1
     end
     plot(all_epsk,w_epsk(idx,:),'.k',markersize=8,linewidth=2)
     xlabel('$\varepsilon_{\kappa}$','Interpreter','latex','fontsize',18)
-    ylabel('$\omega$','Interpreter','latex','fontsize',18)
+    ylabel('$\mathrm{Im}(\omega)$','Interpreter','latex','fontsize',18)
 
 else
 
     for idx = 1:2*N
 
+        val_old = 0;
         subplot(2,N,idx)
         hold on
 
@@ -182,7 +223,7 @@ else
         end
         plot(all_epsk,w_epsk(idx,:),'.k',markersize=8,linewidth=2)
         xlabel('$\varepsilon_{\kappa}$','Interpreter','latex','fontsize',18)
-        ylabel('$\omega$','Interpreter','latex','fontsize',18)
+        ylabel('$\mathrm{Im}(\omega)$','Interpreter','latex','fontsize',18)
         
 %         for k = 1:2*N
 %             plot(all_epsk,w_epsk(k,:),'.k',markersize=8,linewidth=2)
@@ -237,6 +278,7 @@ function [w_res] = omega_epsk(epsilon_kappa,li,Omega,phase_kappa,delta,vr,v0,lij
     if N > 1
         C = make_capacitance_finite(N,lij); % capacitance matrix
         w_res = get_capacitance_approx_hot(epsilon_kappa,li,Omega,phase_kappa,delta,C,vr,v0); % subwavelength resonant frequencies
+        [l,i] = sort(imag(w_res)); w_res = w_res(i); % sort omegas according to the real part
 %         w_res = w_res(real(w_res)>=0); % positive subwavelength resonant frequencies
     else
         w_res = [get_capacitance_approx_spec_im_N1_1D(epsilon_kappa,Omega,li,delta,vr,v0),0]; % non-zero subwavelength resonant frequency
