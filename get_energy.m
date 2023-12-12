14a71b4db7f1a47fcf00c78c224cd4ed79124a91
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2023-12-12 11:58:58 +0100

    new function to compute the total energy of a time-modulated system

---
+++ get_energy.m
@@ -0,0 +1,21 @@
+function [tot_en] = get_energy(tns,rns,w_op,Omega,v0,xm,xp,k_tr,t)
+%GET_ENERGY Computes the energy of a system using the transmission and reflection coefficient of each mode
+%   tns:    transmission coefficient of each mode
+%   rns:    reflection coefficients of each mode
+%   w_op:   operating wave frequency
+%   Omega:  time-modulation frequency
+%   v0:     wave speed outside of the resonators
+%   xm:     left boundary of the first resonator
+%   xp:     right boundary of the last resonator
+%   k_tr:   truncation parameter
+%   t:      time
+
+    r_sum = 0; t_sum = 0;
+    w_i = imag(w_op);
+    for n = -k_tr:k_tr
+        r_sum = r_sum + rns(n+k_tr+1)*exp(-sqrt(-1)*n*Omega/v0*xm)*exp(sqrt(-1)*n*Omega*t);
+        t_sum = t_sum + tns(n+k_tr+1)*exp(-sqrt(-1)*n*Omega/v0*xp)*exp(sqrt(-1)*n*Omega*t);
+    end
+    tot_en = abs(exp(-w_i*t)).^2.*(abs(exp(w_i*xm/v0)).^2.*abs(r_sum).^2+abs(exp(w_i*xp/v0)).^2.*abs(t_sum).^2);
+
+end
