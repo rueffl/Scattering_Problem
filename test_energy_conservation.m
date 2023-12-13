7213e628688ff3ed93451c272a215bbe34935f57
Liora Rueff <liora.rueff@sam.math.ethz.ch>
2023-12-13 18:10:28 +0100

    added tests reganrding time-dependency of energy

--- test_energy_conservation.m
+++ test_energy_conservation.m
@@ -3,7 +3,7 @@ format long
 
 % Settings for the material's structure
 k_tr = 4; % truncation parameters as in remark 3.3
-N = 6; % number of the resonator
+N = 1; % number of the resonator
 spacing = 10; lij = ones(1,N-1).*spacing; % spacing between the resonators
 len = 2; li = ones(1,N).*len; % length of the resonator
 L = sum(li)+sum(lij); % length of the unit cell
@@ -56,7 +56,7 @@ O = zeros(2*k_tr+1,length(all_w)); E = zeros(1,length(all_w)); i = 1;
 % hold on
 c_map = parula(length(all_w)+2);
 
-for w_op = all_w(2:end)
+for w_op = all_w(1:end)
     
     k_op = w_op/v0; % operating wave number outside of the resonator
     w0 = w_op; k0 = k_op;
@@ -97,13 +97,16 @@ legend('show',interpreter='latex',fontsize=18)
 
 figure()
 hold on
-plot(all_w(2:end),E(1:end-1),'o','MarkerSize',8,'LineWidth',2)
+plot(all_w,E(1:end),'o','MarkerSize',8,'LineWidth',2)
+for w = w_res
+    plot(w.*ones(1,100),linspace(min(E(1:end)),max(E(1:end)),100),'--r','MarkerSize',8,'LineWidth',2)
+end
 xlabel('$\omega$',Interpreter='latex',FontSize=18)
 ylabel('Total Energy',Interpreter='latex',FontSize=18)
 
 %% Iterate over epsilon_kappa
 
-all_epsk = [0.1,0.2,0.3,0.4,0.5,0.6];
+all_epsk = linspace(0,1.8,19);
 O = zeros(2*k_tr+1,length(all_epsk)); E = zeros(1,length(all_epsk)); ic = 1;
 fig = figure();
 % hold on
@@ -179,5 +182,85 @@ hold on
 plot(all_epsk,E,'o','MarkerSize',8,'LineWidth',2)
 xlabel('$\varepsilon_{\kappa}$',Interpreter='latex',FontSize=18)
 ylabel('Total Energy',Interpreter='latex',FontSize=18)
+
+
+%% Test energy conservation over time
+
+all_epsk = linspace(0,1.8,19);
+ts_cell = linspace(0,T,200);
+ts = [ts_cell,ts_cell+T];
+E = zeros(length(all_epsk),length(ts)); it = 1; ik = 1;
+fig = figure();
+hold on
+c_map = parula(length(all_epsk)+2);
+
+for epsilon_kappa = all_epsk
+    
+    % Setting for the material's time-modulation
+    Omega = 0.03; % modulation frequency
+    T = 2*pi/Omega;
+    phase_kappa = zeros(1,N); % modulation phases of kappa
+    phase_rho = zeros(1,N); % modulation phases of rho
+    for i = 1:(N-1)
+        phase_kappa(i+1) = pi/i;
+        phase_rho(i+1) = pi/i;
+    end
+    epsilon_rho = 0; % modulation amplitude of rho
+    rs = []; % Fourier coefficients of 1/rho
+    ks = []; % Fourier coefficients of 1/kappa
+    for j = 1:N
+        rs_j = [epsilon_rho*exp(-1i*phase_rho(j))./2,1,epsilon_rho*exp(1i*phase_rho(j))./2];
+        ks_j = [epsilon_kappa*exp(-1i*phase_kappa(j))./2,1,epsilon_kappa*exp(1i*phase_kappa(j))./2];
+        ks = [ks; ks_j];
+        rs = [rs; rs_j];
+    end
+    
+    % Calculate subwavelength resonant frequency
+    if N == 1
+        w_res = get_capacitance_approx_spec_im_N1_1D(epsilon_kappa,Omega,len,delta,vr,v0); % non-zero subwavelength resonant frequency
+    else
+        C = make_capacitance_finite(N,lij); % capacitance matrix
+        w_muller = get_capacitance_approx_hot(epsilon_kappa,li,Omega,phase_kappa,delta,C,vr,v0); % subwavelength resonant frequencies
+        w_res = w_muller(real(w_muller)>=0); % positive subwavelength resonant frequencies
+    end
+    w_op = w_res(1)+0.0002; w0 = w_op;
+    k_op = w_op/v0; k0 = w0/v0;
+    
+    
+    % Define incident wave field  
+    vin_l = @(x,n) 1*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % n-th mode of left incident wave
+    dx_vin_l = @(x,n) 1*sqrt(-1)*k0*exp(sqrt(-1)*(k0).*x).*(x<xm(1)).*(n==0); % derivative of left incident wave
+    uin_r = @(x,t,n) 0; vin_r = @(x,n) 0; dx_vin_r = @(x,n) 0;
+    
+    % Build the linear system and solve it for the interior coefficients
+    MatcalA = getMatcalA(N,lij,xm,xp,k_tr,w_op,Omega,rs,ks,vr,delta,v0); % matrix \mathcal{A}
+    MatcalF = getF_lr(k_tr, N, delta, xm, xp, dx_vin_l, dx_vin_r); % vector \mathcal{F}
+    N_vin = getN_vin(k_tr, N, delta, xm, xp, w_op, Omega, v0, lij, vin_l, vin_r); % matrix-vector product \mathcal{N}v^{in}
+    RHS = MatcalF + N_vin;
+    sol = MatcalA\RHS; % solve for the interior coefficients, vector \mathbf{w}
+
+    it = 1;
+    for t = ts
+        % Compute the transmission and reflection coefficients
+        [tns,rns] = get_tr(k_tr, w_op, Omega, rs, ks, vr, xm, xp, sol, lij, vin_l, vin_r);
+        E(ik,it) = get_energy(tns(:,1),rns(:,end),w_op,Omega,v0,xm(1),xp(end),k_tr,t);
+        it = it+1;
+    end
+    ik = ik + 1;
+
+    % Plot results
+    plot(ts,E(ik-1,:),'-','Color',c_map(ik-1,:),'DisplayName',strcat('$\varepsilon_{\kappa}=\,\,$',num2str(epsilon_kappa)),'MarkerSize',8,'LineWidth',2)
+
+end
+
+xlabel('$t$',Interpreter='latex',FontSize=18)
+ylabel('Total Energy',Interpreter='latex',FontSize=18)
+legend('show',interpreter='latex',fontsize=18)
+
+% Check energy change
+D1 = E(:,1)-E(:,200);
+D2 = E(:,201)-E(:,end);
+
+
 
 
